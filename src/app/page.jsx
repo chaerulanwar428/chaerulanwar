bd38e2e2baf8d1c5285d35c454724e402467451d
unknown <chaerulanwar428@gmail.com>
2024-10-20 22:20:43 +0700

    ganti gambar profile

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,7 +16,7 @@ const Homepage = () => {
       <div className="h-full flex flex-col lg:flex-row px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48">
         {/* IMAGE CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 relative">
-          <div className="hero_img"></div>
+          <Image src="/profile.png" alt="" fill className="object-contain" />
         </div>
         {/* TEXT CONTAINER */}
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
