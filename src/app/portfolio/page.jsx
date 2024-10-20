2cdab19d8462a3a214c2917f5b0788a3a4f4b1f7
unknown <chaerulanwar428@gmail.com>
2024-10-21 00:49:11 +0700

    ubah ukuran

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -55,13 +55,13 @@ const PortfolioPage = () => {
             {items.map((item) => (
               <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id}>
                 <div className="flex flex-col gap-8 text-white">
-                  <h1 className="text-xl font-bold md:text-4xl lg:text-6xl xl:text-8xl">{item.title}</h1>
+                  <h1 className="text-xl font-bold md:text-3xl lg:text-5xl xl:text-6xl">{item.title}</h1>
                   <div className="relative w-80 h-56 md:w-96 md:h-64 lg:w-[500px] lg:h-[350px] xl:w-[600px] xl:h-[420px]">
                     <Image src={item.img} alt="" fill />
                   </div>
                   <p className="w-80 md:w96 lg:w-[500px] lg:text-lg xl:w-[600px]">{item.desc}</p>
                   <Link href={item.link} className="flex justify-end">
-                    <button className="p-2 text-sm md:p-4 md:text-md lg:p-8 lg:text-lg bg-white text-gray-600 font-semibold m-4 rounded">See Demo</button>
+                    <button className="p-1 text-sm md:p-4 md:text-md lg:p-8 bg-white text-gray-600 font-semibold m-2 rounded">See Demo</button>
                   </Link>
                 </div>
               </div>
