28de7f881e0dfddd90b80c46001713532f967e5f
unknown <chaerulanwar428@gmail.com>
2024-10-20 13:28:47 +0700

    efek nama

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { motion } from "framer-motion";
 
 const Homepage = () => {
+  const text = "Chaerul Anwar";
   return (
     <motion.div
       className="h-full"
@@ -20,7 +21,22 @@ const Homepage = () => {
         <div className="h-1/2 lg:h-full lg:w-1/2 flex flex-col gap-8 items-center justify-center">
           {/* TITLE */}
           <h1 className="text-3xl md:text-6xl font-bold">
-            Hello, I am Chaerul anwar a Fullstack Developer
+            Hello, I am
+            {text.split("").map((letter, index) => (
+              <motion.span
+                key={index}
+                initial={{ opacity: 1 }}
+                animate={{ opacity: 0 }}
+                transition={{
+                  duration: 3,
+                  repeat: Infinity,
+                  delay: index * 0.1,
+                }}
+              >
+                {letter}
+              </motion.span>
+            ))}
+            <br/> a Fullstack Developer
           </h1>
           {/* DESC */}
           <p className="md:text-xl">
